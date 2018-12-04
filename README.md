70deda5a2bf6cd118d0113e2cdb020a09006fb53
Łukasz Kurowski <crackcomm@gmail.com>
2018-12-04 02:55:32 +0100

    feat(*): first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# [go-ipfn-codecs][IPv8]: IPFN common codecs registry
+
+[![IPFN project][badge-ipfn]][org-ipfn]
+[![IPFN Documentation][badge-docs]][docs]
+[![See COPYING.txt][badge-copying]][COPYING]
+[![GoDoc][badge-godoc]][godoc-ipfn]
+[![Travis CI][badge-ci]][ci]
+[![Coverage Status][coverage-badge]][coverage-status]
+
+Common codecs registry.
+
+## License
+
+See [COPYING][COPYING] file for licensing details.
+
+## Project
+
+This source code is part of [IPFN](https://github.com/ipfn) – interplanetary functions project.
+
+[ci]: https://travis-ci.org/ipfn/go-ipfn-codecs
+[docs]: https://docs.ipfn.io/
+[COPYING]: https://github.com/ipfn/go-ipfn-codecs/blob/master/COPYING
+[badge-ci]: https://travis-ci.org/ipfn/go-ipfn-codecs.svg?branch=master
+[badge-copying]: https://img.shields.io/badge/license-Apache%202.0-blue.svg?style=flat-square
+[badge-docs]: https://img.shields.io/badge/documentation-IPFN-blue.svg?style=flat-square
+[badge-godoc]: https://godoc.org/github.com/ipfn/go-ipfn-codecs/codecs?status.svg
+[badge-ipfn]: https://img.shields.io/badge/project-IPFN-blue.svg?style=flat-square
+[coverage-badge]: https://coveralls.io/repos/github/ipfn/go-ipfn-codecs/badge.svg?branch=master
+[coverage-status]: https://coveralls.io/github/ipfn/go-ipfn-codecs?branch=master
+[org-ipfn]: https://github.com/ipfn
+[godoc-ipfn]: https://godoc.org/github.com/ipfn/go-ipfn-codecs/codecs
+[codecs]: https://github.com/ipfn/go-ipfn-codecs/
---
+++ codecs/codecs.go
@@ -0,0 +1,51 @@
+// Copyright © 2017-2018 The IPFN Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package codecs registers IPFN codecs in CID codec types.
+package codecs
+
+type target struct {
+	Codecs     map[string]uint64
+	CodecToStr map[uint64]string
+}
+
+var targets []target
+
+// Codecs - Maps the name of a codec to its type.
+var Codecs = make(map[string]uint64)
+
+// CodecToStr - Maps the numeric codec to its name.
+var CodecToStr = make(map[uint64]string)
+
+// Register - Registers codecs in remote cids package.
+func Register(codecs map[string]uint64) {
+	for _, target := range targets {
+		for name, codec := range codecs {
+			target.Codecs[name] = codec
+			target.CodecToStr[codec] = name
+		}
+	}
+}
+
+// RegisterTarget - Registers codecs in remote cids package.
+func RegisterTarget(codecs map[string]uint64, codecToStr map[uint64]string) {
+	targets = append(targets, target{
+		Codecs:     codecs,
+		CodecToStr: codecToStr,
+	})
+	for name, codec := range Codecs {
+		codecs[name] = codec
+		codecToStr[codec] = name
+	}
+}
